0bb46763dbee3336c1090e849a8514d3fd3ffa30
Sarmistha <sarmi25012004@gmail.com>
2025-07-17 23:31:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ st.set_page_config(page_title="📊 Relatrix - Corporate CRM Dashboard", layout=
 st.markdown("<h1 style='text-align: center;'>🔐 Welcome to Relatrix CRM</h1>", unsafe_allow_html=True)
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("logo.png", width=180)
+    st.image("logo.png", width=220)
 
 # 🌈 Background and Hide Header/Footer
 st.markdown("""
