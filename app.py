ee5f662d648c6226541f5016258823d3937acd03
Sarmistha <sarmi25012004@gmail.com>
2025-07-18 00:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -277,11 +277,11 @@ if st.session_state.page == "auth":
 # Dashboard Page (Post-login)
 # ---------------------------
 if st.session_state.page == "dashboard" and st.session_state.logged_in:
-    st.markdown("<h1 style='text-align: center';'font-size: 44px; color:#6C63FF; font-family:monospace;'>📊 Relatrix</h1">, unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; font-size: 44px; color:#6C63FF; font-family:monospace;'>📊 Relatrix</h1">, unsafe_allow_html=True)
     logo = Image.open("logo.png")
     st.image(logo, width=250)
-    st.markdown("<p style='text-align: center';'font-size: 24px; color: deeppink; font-family: 'Comic Sans MS', cursive; font-weight: bold;'>📈 Where Relationships Drive Results.</p>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center';'font-size: 22px; font-family: 'Comic Sans MS', cursive; font-weight: bold;'>🏢 A Corporate CRM Dashboard</p>",unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; font-size: 24px; color: deeppink; font-family: 'Comic Sans MS', cursive; font-weight: bold;'>📈 Where Relationships Drive Results.</p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; font-size: 22px; font-family: 'Comic Sans MS', cursive; font-weight: bold;'>🏢 A Corporate CRM Dashboard</p>",unsafe_allow_html=True)
             
     col1, col2 = st.columns([6, 1])
     with col1:
